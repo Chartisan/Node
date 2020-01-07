7a388aa89bdcc14889291d907fdc1b2da9f79504
Erik Campobadal <soc@erik.cat>
2020-01-07 03:29:24 +0100

    Example simplified

--- example/src/example.js
+++ example/src/example.js
@@ -1,25 +1,16 @@
 const polka = require('polka')
 const { Chartisan } = require('@chartisan/node')
 
-/**
- * Main function for the chart.
- *
- * @param {*} req
- * @returns
- */
-function chart(req) {
-    return Chartisan.build()
-        .labels(['a', 'b', 'c'])
-        .dataset('Sample 1', [1, 2, 3])
-        .dataset('Sample 2', [3, 2, 1])
-        .toJSON()
-}
-
 // Initiate the HTTP server
 polka()
     .get('/', (req, res) => {
         res.setHeader('Content-Type', 'application/json')
-        res.end(chart(req))
+        const chart = Chartisan.build()
+            .labels(['a', 'b', 'c'])
+            .dataset('Sample 1', [1, 2, 3])
+            .dataset('Sample 2', [3, 2, 1])
+            .toJSON()
+        res.end(chart)
     })
     .listen(3000, err => {
         if (err) throw err
